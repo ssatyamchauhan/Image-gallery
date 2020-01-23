06703e6c5f1634f2985d5b88928e196d3278c1c6
ssatyamchauhan <ssatyamchauhan1998@gmail.com>
2020-01-23 14:02:16 +0530

    image-gallery using JLayout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Gallery } from './components/Gallery/Gallery';
-import { Link } from "./components/Link/Link";
+import Gallery from './components/Gallery/Gallery';
+// import { Link } from "./components/Link/Link";
 import galleryData from "./mockData/gallery.json"
+
 
 export const App = () => {
   return (
     <div className="App">
-      <h1>Justified Gallery</h1>
+      {/* <h1>Justified Gallery</h1>
       <h2>Intro</h2>
       <p>Often, a marketplace like <Link href="https://matrimonybazaar.com">ours</Link> requires a gallery component that makes the photos the hero of the page.</p>
       <p>A <Link href="https://www.flickr.com/search/?text=sunrise">justified gallery</Link> like the one <Link href="https://flickr.com" >flickr</Link> popularised makes it easy to display images with varying aspect ratios.
@@ -16,9 +17,10 @@ export const App = () => {
       <h2>Problem Statement</h2>
       <p>Write a component that takes a list of URLs like the one below to create a justified gallery.</p>
       <p>The component to be implemented is in <code>components/Gallery/Gallery.jsx</code></p>
-      <p>The data is available in the <code>mockData/gallery.json</code></p>
+      <p>The data is available in the <code>mockData/gallery.json</code></p> */}
       <Gallery data={galleryData} />
     </div>
+
   );
 }
 
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,13 +1,77 @@
-import React from 'react';
-
-
-/* 
+import React, { useEffect, useState } from "react";
+import JLayout from "justified-layout";
+import "../../App.css";
+/*
  * Make this component
  */
-export const Gallery = ({ data }) => {
+const Geometries = [
+  0.5,
+  1.5,
+  1,
+  1.8,
+  0.4,
+  0.7,
+  0.9,
+  1.1,
+  1.7,
+  2.2,
+  1.5,
+  0.5,
+  1.5,
+  1,
+  1.8
+];
+
+export default function Gallery(props) {
+
+  const [state, setState] = useState({
+    containerWidth: 0,
+    geometry: { boxes: [] }
+  });
+
+  let resizeGeometry = () => {
+    let width = window.innerWidth - 16; // padding
+
+    setState({
+      ...state,
+      containerWidth: width,
+      geometry: JLayout(Geometries, { containerWidth: width })
+    });
+  };
+
+  useEffect(() => {
+    resizeGeometry();
+
+    window.addEventListener("resize", resizeGeometry);
+  }, []);
+
+  let boxes = state.geometry.boxes.map((box, i) => {
+    let style = {
+      left: box.left,
+      top: box.top,
+      width: box.width,
+      height: box.height
+    };
+    return (
+      <img
+        key={i}
+        className="box"
+        style={style}
+        src={props.data[i].url}
+        alt={props.data[i].alt}
+      />
+    );
+  });
+
   return (
-    <div>
-      <pre>{JSON.stringify(data.map((image) => image.url), null, 2)}</pre>
+    <div
+      className="wrapper"
+      style={{
+        height: state.geometry.containerHeight + "px",
+        width: state.containerWidth + "px"
+      }}
+    >
+      {boxes}
     </div>
   );
 }
